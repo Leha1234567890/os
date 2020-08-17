3cd5c635d9c2552d95287b57da311efbe24223b0
Leha1234567890 <66202653+Leha1234567890@users.noreply.github.com>
2020-08-17 18:21:38 +0300

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -1,59 +1,78 @@
 #include <conio.h>
 #include <cstdlib>
+#include <cstdio>
 
 #define ESC 27
 
 int main() {
     while (true) {
+        printf("$ ");
         switch (_getch()) {
             case 'c':
+                printf("c\n\n");
                 system("cmd");
                 break;
             case 's':
+                printf("s\n\n");
                 system("\"C:\\Program Files (x86)\\Microsoft\\Skype for Desktop\\Skype.exe\"");
                 break;
             case 'v':
-                system("\"C:\\Users\\Îëüãà\\AppData\\Local\\Viber\\Viber.exe\"");
+                printf("v\n\n");
+                system("\"C:\\Users\\ÃŽÃ«Ã¼Ã£Ã \\AppData\\Local\\Viber\\Viber.exe\"");
                 break;
             case 'f':
+                printf("f\n\n");
                 system("\"C:\\Program Files (x86)\\Mozilla Firefox\\Firefox.exe\"");
                 break;
             case 'p':
+                printf("p\n\n");
                 system("powershell");
                 break;
             case 'w':
+                printf("w\n\n");
                 system("\"C:\\Program Files\\LibreOffice 5\\program\\swriter.exe\"");
                 break;
             case 'm':
+                printf("m\n\n");
                 system("\"C:\\Program Files\\LibreOffice 5\\program\\smath.exe\"");
                 break;
             case 'd':
+                printf("d\n\n");
                 system("\"C:\\Program Files\\LibreOffice 5\\program\\sdraw.exe\"");
                 break;
             case 'i':
+                printf("i\n\n");
                 system("\"C:\\Program Files\\LibreOffice 5\\program\\simpress.exe\"");
                 break;
             case 'b':
+                printf("b\n\n");
                 system("\"C:\\Program Files\\LibreOffice 5\\program\\scalc.exe\"");
                 break;
             case 'D':
+                printf("D\n\n");
                 system("\"C:\\Program Files\\Inkscape\\inkscape.exe\"");
                 break;
             case 'C':
+                printf("C\n\n");
                 system("\"C:\\Windows\\system32\\calc.exe\"");
                 break;
             case 'P':
+                printf("P\n\n");
                 system("python");
                 break;
             case 't':
+                printf("t\n\n");
                 system("notepad");
                 break;
             case 'T':
+                printf("T\n\n");
                 system("C:\\Users\\Leha\\Documents\\Sublime\\sublime_text.exe");
                 break;
             case 'q':
+                printf("Quit");
                 return 0;
             case ESC:
+                printf("Quit");
                 return 0;
         }
     }
